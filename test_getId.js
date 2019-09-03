8e10b8079d64725c2637d67077f0c2239472f0ae
alex <you@example.com>
2019-09-03 13:07:45 -0400

    update

--- test_getId.js
+++ test_getId.js
@@ -13,7 +13,7 @@ col_names.forEach(name => {
             return
         }
         let results = response.results
-        
+
         // extract ids from results
         let ids = []
         results.forEach(function (result) {
@@ -21,21 +21,38 @@ col_names.forEach(name => {
             ids.push(id);
         })
 
+        let found_entities = []
         wikidataSearch.getEntities(ids, true, function (response, error) {
             if (error) {
                 console.log("getEntities error:", error)
                 return;
             }
             let entities = response.entities;
-            for (let i = 0; i < ids.length; i++) {
+            let isEntityFound = false
+            for (let i = 0; i < entities.length; i++) {
                 // let id = ids[i];
                 let entity = entities[i];
                 let claims = entity["claims"]
-                claims.forEach(claim=>{
+                for (let j = 0; j < claims.length; j++) {
+                    let claim = claims[j]
                     let property = claim["property"]
                     let value = claim["value"]
-                    if (property=="instance of" && value=="")
-                })
+                    if (property == "instance of") {
+                        if (value == "film") {
+                            // entity
+                            console.log("film entity is:", entity)
+                            found_entities.push(entity)
+                            isEntityFound = true;
+                            if (isEntityFound) {
+                                break;
+                            }
+                        }
+                    }
+
+                }
+                if (isEntityFound) {
+                    break;
+                }
                 // console.log(entity)
             }
         })
